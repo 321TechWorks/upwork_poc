8ac4b3f062c0c42560a148755810129afe53d057
Oleksandr Poplavskyi <oleksandrpoplavskyi@oleksandr.local>
2024-04-11 22:16:13 +0300

    create root layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,12 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
+    colors: {
+      white: "#fff",
+      lightGray: "#F9FAFB",
+      gray: "#545454",
+      red: "#bf360c",
+    },
     extend: {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
